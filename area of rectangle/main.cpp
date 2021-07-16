63602aaf7a1f2ce9f0597575a182e9134e6a2885
Yahya mallak <Yahya10mallak@gmail.com>
2021-07-17 00:31:46 +0100

    area of rectangle project

---
+++ area of rectangle/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int width, length, area;
+
+    cout << "Please enter a width & a length: " << endl;
+    cin >> width >> length;
+
+    area = width * length;
+
+    cout << "Area: " << area;
+
+
+    return 0;
+}
