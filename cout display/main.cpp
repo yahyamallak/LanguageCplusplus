ddc63aac5ed14fd087ec7daacab6abdc4053ab8e
Yahya mallak <Yahya10mallak@gmail.com>
2021-12-27 03:37:24 +0100

    sum two numbers program

---
+++ cout display/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main()
+{
+    
+    std::cout << "Subject\tMarks\n"
+    << "Mathematics\t90\n"
+    << "Computer\t77\n"
+    << "Chemistry\t69\n"
+    ;
+
+
+    return 0;
+}
---
+++ two numbers sum/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int a, b;
+
+    std::cout << "Enter Two numbers : " << std::endl;
+    std::cin >> a >> b;
+    std::cout << "The Sum of the Two numbers : " << std::endl;
+    std::cout << a + b << std::endl;
+
+    return 0;
+}
