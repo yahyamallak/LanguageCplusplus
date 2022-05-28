9de6096f09eb3f1747f4c3ccbb5a54083da3c44c
Yahya mallak <Yahya10mallak@gmail.com>
2022-05-29 00:31:34 +0100

    add getline string program

---
+++ getline string/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    string s;
+
+    cout << "Enter a string : ";
+    getline(cin, s);
+
+    cout << "String : " << s;
+
+
+    return 0;
+}
