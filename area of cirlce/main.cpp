55db7e8b115740b0cb443fbfd18c48fa0e8d03d4
Yahya mallak <Yahya10mallak@gmail.com>
2022-05-29 00:29:24 +0100

    add area of circle program

---
+++ area of cirlce/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int r;
+    float PI = 3.14;
+
+    cout << "Enter the radius : ";
+    cin >> r;
+
+    cout << "The area of the circle is : " << (2*PI*r);
+
+    return 0;
+}
