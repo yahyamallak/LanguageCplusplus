9f082ed159a7cfcd18927756c462b25082574373
Yahya mallak <Yahya10mallak@gmail.com>
2022-06-18 01:53:26 +0100

    interest rate program

---
+++ interest rate/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    float amount;
+
+    cout << "Enter the amount : ";
+    cin >> amount;
+
+    cout << "The result : " << amount * 0.2;
+}
