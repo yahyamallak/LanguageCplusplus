e64f198f8e6e7440d15168d527e77e2c18c76865
Yahya mallak <Yahya10mallak@gmail.com>
2022-06-18 01:52:19 +0100

    average of 3 nums program

---
+++ average of 3 nums/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    int a, b, c;
+
+    cout << "Enter number 1 : ";
+    cin >> a;
+
+    cout << "Enter number 2 : ";
+    cin >> b;
+
+    cout << "Enter number 3 : ";
+    cin >> c;
+
+
+    cout << "The average : " << (a+b+c)/3;
+
+}
