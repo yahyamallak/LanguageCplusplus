e019321bee63d8fc7dc2fa81894f57e9836ebf53
Yahya mallak <Yahya10mallak@gmail.com>
2022-06-18 01:51:28 +0100

    area of triangle program

---
+++ area of triangle/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int base, height;
+
+    cout << "Enter the base : ";
+    cin >> base;
+
+    cout << "Enter the height : ";
+    cin >> height;
+
+    cout << "The Area : " << (base * height)/2;
+}
