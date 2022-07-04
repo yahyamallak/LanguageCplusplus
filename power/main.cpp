44b65f7cd0896333792d09363ff329cbd5feb247
Yahya mallak <Yahya10mallak@gmail.com>
2022-07-04 16:18:04 +0100

    add power program

---
+++ power/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int a, n;
+
+    cout << "Enter a number : ";
+    cin >> a;
+
+    cout << "Enter a power : ";
+    cin >> n;
+
+    cout << "A power N : " << pow(a,n);
+
+    return 0;
+}
