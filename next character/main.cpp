dc65f7baccb8bbde2cd8cc6c7b04bf1171cbb82b
Yahya mallak <Yahya10mallak@gmail.com>
2022-07-04 16:43:57 +0100

    add next character program

---
+++ next character/main.cpp
@@ -0,0 +1,24 @@
+/*
+ A
+ next : B
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    char letter;
+
+    cout << "Enter a letter : ";
+    cin >> letter;
+
+    letter = letter + 1;
+
+    cout << "The next letter : " << letter;
+
+    return 0;
+}
