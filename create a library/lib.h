b1d297f5cce71b061cf0be15245a7fb927c9fea2
Yahya mallak <Yahya10mallak@gmail.com>
2022-09-15 22:45:33 +0100

    add create a library

---
+++ create a library/lib.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+namespace Lib {
+
+    int sumTwoNums(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
---
+++ create a library/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+using namespace Lib;
+
+
+int main() {
+
+    cout << sumTwoNums(10, 5);
+
+    return 0;
+}
