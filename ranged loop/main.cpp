2ef07231d9ff5970d085fda518b987a39484258d
Yahya mallak <Yahya10mallak@gmail.com>
2022-09-16 03:01:28 +0100

    add ranged loop

---
+++ ranged loop/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int array[] = {1, 2, 3, 4, 5};
+
+    for(int num : array) {
+        cout << num << endl;
+    }
+
+    return 0;
+}
