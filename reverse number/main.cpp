0f52240dd49ea0e07afb01eae3baed098baba643
Yahya mallak <Yahya10mallak@gmail.com>
2023-03-03 04:15:06 +0100

    add reverse number program

---
+++ reverse number/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int ReadPositiveNumber(string Message) {
+    int Number;
+
+    do {
+
+        cout << Message << endl;
+        cin >> Number;
+
+    } while(Number < 0);
+
+    return Number;
+}
+
+
+
+int ReveseNumber(int Number) {
+    
+    int newNumber = 0;
+
+    while(Number) {
+        newNumber = ( Number % 10 ) + newNumber * 10;
+        Number = Number / 10;
+    }
+    
+    return newNumber;
+}
+
+
+int main() {
+
+
+    cout << ReveseNumber(ReadPositiveNumber("Enter a positive number ? "));
+
+
+    return 0;
+}
