5e294278c8b109c233bf6b44eb95e6726ab4c551
Yahya mallak <Yahya10mallak@gmail.com>
2023-03-04 02:31:35 +0100

    add inverted pattern number program

---
+++ inverted pattern number/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int ReadPositiveNumber(string Message) {
+    int Number;
+
+    do {
+
+        cout << Message;
+        cin >> Number;
+
+    } while(Number < 0);
+
+    return Number;
+}
+
+
+
+void PrintInvertedPattern(int Number) {
+
+    for(int i = Number; i >= 1 ; i--) {
+        for(int j = 1; j <= i; j++) {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+
+
+int main() {
+
+    PrintInvertedPattern(ReadPositiveNumber("Please enter a positive number ? "));
+
+    return 0;
+}
