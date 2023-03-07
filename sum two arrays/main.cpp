2d7408deec0ea55c19fa537ee92464dcc2024086
Yahya mallak <Yahya10mallak@gmail.com>
2023-03-07 04:40:36 +0100

    add sum two arrays program

---
+++ sum two arrays/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <array>
+
+
+using namespace std;
+
+
+int ReadPositiveNumber(string Message) {
+    int Number;
+
+    do {
+
+        cout << Message << endl;
+        cin >> Number;
+
+    } while(Number < 0);
+
+    return Number;
+}
+
+
+int RandomNumber(int from, int to) {
+
+    int number = rand() % (to - from + 1) + from;
+
+    return number;
+}
+
+void SumArrays(int arr3[100], int arr[100], int arr2[100], int & arrLength) {
+    for(int i = 0; i < arrLength; i++) {
+        arr3[i] = arr[i] + arr2[i];
+    }
+}
+
+
+void FillArray(int arr[100], int & arrLength) {
+
+    for(int i = 0; i < arrLength; i++) {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+
+
+
+void PrintArray(int arr[100], int arrLength) {
+    for(int i = 0; i < arrLength; i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+
+
+int main() {
+
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength;
+
+    do {
+
+        arrLength = ReadPositiveNumber("Enter a positive number : ");
+
+    } while(arrLength < 0 || arrLength > 100);
+
+    FillArray(arr, arrLength);
+
+    cout << "Array 1 elements : ";
+    PrintArray(arr, arrLength);
+
+    int arr2[100];
+
+    FillArray(arr2, arrLength);
+
+    cout << "Array 2 elements : ";
+    PrintArray(arr2, arrLength);
+
+    int arr3[100];
+
+    SumArrays(arr3, arr, arr2, arrLength);
+
+    cout << "Sum of arrays : ";
+    PrintArray(arr3, arrLength);
+
+    return 0;
+}
