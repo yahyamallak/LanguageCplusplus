609c9cd3aa3bb6d081deecd30928e3b1104c7ddd
Yahya mallak <Yahya10mallak@gmail.com>
2023-03-10 03:48:04 +0100

    add count negative nums array program

---
+++ count negative nums array/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <array>
+
+
+using namespace std;
+
+
+enum enNumberSign { Positive = 1, Negative = 2};
+
+
+int ReadPositiveNumber(string Message) {
+    int Number;
+
+    do {
+
+        cout << Message << endl;
+        cin >> Number;
+
+    } while(Number < 0);
+
+    return Number;
+}
+
+
+int RandomNumber(int from, int to) {
+
+    int number = rand() % (to - from + 1) + from;
+
+    return number;
+}
+
+enNumberSign CheckNumberSign(int Num) {
+    if(Num >= 0)
+        return enNumberSign::Positive;
+    else
+        return enNumberSign::Negative;
+}
+
+int CountNegativeNumsArray(int arr[100], int arrLength) {
+    
+    int counter = 0;
+    
+    for(int i = 0; i < arrLength; i++){
+        if(CheckNumberSign(arr[i]) == enNumberSign::Negative) {
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
+
+void FillArray(int arr[100], int & arrLength) {
+
+    for(int i = 0; i < arrLength; i++) {
+        arr[i] = RandomNumber(-100, 100);
+    }
+}
+
+void PrintArray(int arr[100], int arrLength) {
+    for(int i = 0; i < arrLength; i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+
+
+int main() {
+
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength;
+
+    do {
+
+        arrLength = ReadPositiveNumber("Enter a positive number : ");
+
+    } while(arrLength < 0 || arrLength > 100);
+
+    FillArray(arr, arrLength);
+
+    PrintArray(arr, arrLength);
+
+
+    cout << "Negative Numbers count is : " << CountNegativeNumsArray(arr, arrLength);
+
+
+    return 0;
+}
