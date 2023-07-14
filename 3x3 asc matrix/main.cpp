2bf8822d6950812508ec03256309573b73c50fff
Yahya mallak <Yahya10mallak@gmail.com>
2023-07-14 21:33:07 +0100

    add 3x3 asc matrix program

---
+++ 3x3 asc matrix/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+
+using namespace std;
+
+
+
+void FillMatrix(int matrix[3][3]) {
+
+    int num = 1;
+    
+    for(int i = 0 ; i < 3; i++) {
+        for(int j = 0 ; j < 3; j++) {
+            matrix[i][j] = num++;
+        }
+    }
+}
+
+
+void PrintMatrix(int matrix[3][3]) {
+    
+    for(int i = 0 ; i < 3; i++) {
+        for(int j = 0 ; j < 3; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+}
+
+
+
+int main() {
+
+    srand((unsigned)time(NULL));
+
+    int matrix[3][3];
+
+    FillMatrix(matrix);
+
+    cout << "The following is a 3x3 asc matrix: " << endl;
+
+    PrintMatrix(matrix);
+
+    return 0;
+}
