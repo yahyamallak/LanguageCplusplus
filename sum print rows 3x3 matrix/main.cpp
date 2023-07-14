ea6a62f0f862a373bfe9a8061d06c5e9b20b6686
Yahya mallak <Yahya10mallak@gmail.com>
2023-07-14 21:01:55 +0100

    sum print rows 3x3 matrix program

---
+++ sum print rows 3x3 matrix/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+
+using namespace std;
+
+
+
+int RandomNumber(int from, int to) {
+
+    int number = rand() % (to - from + 1) + from;
+
+    return number;
+}
+
+void FillMatrix(int matrix[3][3]) {
+    
+    for(int i = 0 ; i < 3; i++) {
+        for(int j = 0 ; j < 3; j++) {
+            matrix[i][j] = RandomNumber(0, 100);
+        }
+    }
+}
+
+
+void PrintMatrix(int matrix[3][3]) {
+    
+    for(int i = 0 ; i < 3; i++) {
+        for(int j = 0 ; j < 3; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << "\n";
+    }
+}
+
+int SumRowsMatrix(int matrix[3][3], int row) {
+
+    int sum = 0;
+
+    for(int i = 0; i < 3; i++) {
+        sum += matrix[row][i];
+    }
+
+    return sum;
+
+}
+
+void PrintSumRowsMatrix(int matrix[3][3]) {
+
+    int sums[3] = {0};
+    
+    for(int i = 0; i < 3; i++) {
+        sums[i] = SumRowsMatrix(matrix, i);
+    }
+
+    for(int i = 0; i < 3; i++) {
+        cout << "Row " << i + 1 << " sum = " << sums[i] << "\n";
+    }
+
+}
+
+
+
+int main() {
+
+    srand((unsigned)time(NULL));
+
+    int matrix[3][3];
+
+    FillMatrix(matrix);
+
+    cout << "The following is a 3x3 random matrix: " << endl;
+
+    PrintMatrix(matrix);
+
+
+    cout << "The following are the sums of each row in the random matrix: " << endl;
+
+    PrintSumRowsMatrix(matrix);
+
+    return 0;
+}
